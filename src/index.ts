8c8b5f2536af5928cbd0e0ddb98178e5a30e33d5
Jenson Miralles <jenson.miralles@gmail.com>
2022-06-17 15:27:38 +0800

    dd IOFlatlist and withIOFlatlist in index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,15 @@ import InView from './InView';
 import IOContext from './IOContext';
 import IOScrollView from './IOScrollView';
 import withIO from './withIO';
+import IOFlatList from './IOFlatList';
+import withIOFlatList from './withIOFlatList';
 
-export { IntersectionObserver, InView, IOContext, IOScrollView, withIO };
+export {
+  IntersectionObserver,
+  InView,
+  IOContext,
+  IOScrollView,
+  withIO,
+  IOFlatList,
+  withIOFlatList,
+};
